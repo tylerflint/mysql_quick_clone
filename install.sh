09fb07ba8fff8298153f519f79e010759832d71e
Braxton Huggins <braxton@Delorum9-2.local>
2010-04-27 15:38:42 -0600

    create install.sh to install

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+ cp mysql-quick-clone.pl /usr/local/sbin/
+ cp mysql-quick-clone.conf /etc/
+ cp mysql-quick-clone.pl.8 /usr/local/share/man/man8/
+ cp mysql-quick-clone.conf.8 /usr/local/share/man/man8/
+ 
