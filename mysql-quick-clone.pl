afbcb2b70141bb3ce153264eac24bdea2fd6148e
Braxton Huggins <braxton@Delorum9-2.local>
2010-04-27 16:32:55 -0600

    adding license agreement thingy

--- mysql-quick-clone.pl
+++ mysql-quick-clone.pl
@@ -1,4 +1,23 @@
 #!/usr/bin/perl -w
+#
+# Delorum Framework
+#
+# LICENSE
+#
+# This source file is subject to the new BSD license that is bundled
+# with this package in the file LICENSE.txt.
+# It is also available through the world-wide-web at this URL:
+# http://framework.delorum.com/license/new-bsd
+# If you did not receive a copy of the license and are unable to
+# obtain it through the world-wide-web, please send an email
+# to license@delorum.com so we can send you a copy immediately.
+#
+# @category   Delorum
+# @package    Delorum
+# @copyright  Copyright (c) 2009 Delorum Inc. (http://www.delorum.com)
+# @license    http://framework.delorum.com/license/new-bsd     New BSD License
+# @version    1.0
+##
 
 local $| = 1;
 
